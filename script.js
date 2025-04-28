a8a79a826cc23b69a9e778af9072e841bcb81c13
abhi__yang <sharma.abhiyan9866@gmail.com>
2025-04-28 22:47:04 +0545

    Formatting the files and editing script file

--- script.js
+++ script.js
@@ -1,32 +1,38 @@
 const mainContainer = document.querySelector(".main-container");
 const thanksContainer = document.querySelector(".thank-you");
 const submitbtn = document.getElementById("submit");
-const rateAgain= document.getElementById("rate-again");
+const rateAgain = document.getElementById("rate-again");
 const rating = document.getElementById("rating");
 const rates = document.querySelectorAll(".btn");
 let bool = false;
-submitbtn.addEventListener("click",()=>{
-    if(bool){
-            thanksContainer.classList.remove("hidden");
-    mainContainer.classList.add("hidden");
-    }
 
-   
 
-})
 
-rateAgain.addEventListener("click",()=>
-{
-    thanksContainer.classList.add("hidden");
-    mainContainer.classList.remove("hidden");
-})
 
-rates.forEach(rate=>{
+let checkFunction = rates.forEach(rate => {
 
-    rate.addEventListener("click",()=>
-    {
-       rating.innerText = rate.innerText;
-       bool = true;
+    rate.addEventListener("click", () => {
+        rating.innerText = rate.innerText;
+        bool = true;
     })
 
 })
+
+
+
+submitbtn.addEventListener("click", () => {
+    if (bool) {
+        thanksContainer.classList.remove("hidden");
+        mainContainer.classList.add("hidden");
+    }
+})
+
+rateAgain.addEventListener("click", () => {
+    thanksContainer.classList.add("hidden");
+    mainContainer.classList.remove("hidden");
+    rating.innerHTML = "NONE"
+    bool = false;
+    checkFunction();
+
+})
+
