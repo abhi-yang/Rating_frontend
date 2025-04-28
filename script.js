cecf52da076aeaf182c4e05f44eb8b75ca8b6edc
abhi__yang <sharma.abhiyan9866@gmail.com>
2025-04-28 22:26:21 +0545

    Compeleted

---
+++ script.js
@@ -0,0 +1,32 @@
+const mainContainer = document.querySelector(".main-container");
+const thanksContainer = document.querySelector(".thank-you");
+const submitbtn = document.getElementById("submit");
+const rateAgain= document.getElementById("rate-again");
+const rating = document.getElementById("rating");
+const rates = document.querySelectorAll(".btn");
+let bool = false;
+submitbtn.addEventListener("click",()=>{
+    if(bool){
+            thanksContainer.classList.remove("hidden");
+    mainContainer.classList.add("hidden");
+    }
+
+   
+
+})
+
+rateAgain.addEventListener("click",()=>
+{
+    thanksContainer.classList.add("hidden");
+    mainContainer.classList.remove("hidden");
+})
+
+rates.forEach(rate=>{
+
+    rate.addEventListener("click",()=>
+    {
+       rating.innerText = rate.innerText;
+       bool = true;
+    })
+
+})
